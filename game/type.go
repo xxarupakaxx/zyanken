705824fa45cb7b5f1ff9b9a17c985d00e5e27589
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-03-14 21:58:48 +0900

    ConvertResultToStrの実装

--- game/type.go
+++ game/type.go
@@ -1,11 +1,18 @@
 package game
 
 type Zyanken int
+type Result int
 
 const (
 	Gu Zyanken = iota
 	Choki
 	Pa
+)
+
+const (
+	Draw Result = iota
+	Lose
+	Win
 )
 
 func ConvertZyankenToStr(zyanken Zyanken) string {
@@ -16,6 +23,19 @@ func ConvertZyankenToStr(zyanken Zyanken) string {
 		return "✌"
 	case Pa:
 		return "🖐"
+	}
+
+	return ""
+}
+
+func ConvertResultToStr(result Result) string {
+	switch result {
+	case Draw:
+		return "引き分け"
+	case Lose:
+		return "負け"
+	case Win:
+		return "勝ち"
 	}
 
 	return ""
