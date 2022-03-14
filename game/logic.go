abf2cf03d21c9715887c66c6768476ebd1334e76
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-03-14 22:21:58 +0900

    zyankenLogicの実装

--- game/logic.go
+++ game/logic.go
@@ -1,15 +1,16 @@
 package game
 
-import "fmt"
-
 type Game struct {
 	started  bool
 	finished bool
+	te       Zyanken
 }
 
-func (g *Game) Display(te Zyanken) {
-	fmt.Println("")
-	fmt.Printf("Your Te: %s\n", ConvertZyankenToStr(te))
-	fmt.Println("ーーーーーーーーーーーーーー")
 
+
+
+func zyankenLogic(watashi, aite Zyanken) Result {
+	ans := (watashi - aite + 3) % 3
+
+	return Result(ans)
 }
