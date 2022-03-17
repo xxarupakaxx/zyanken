c44e737947e85c8d63e7d4299861b01cb8ae400c
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-03-17 20:35:11 +0900

    型の変更

--- server/router/game.go
+++ server/router/game.go
@@ -32,12 +32,12 @@ func (g *GameHandler) Play(server pb.ZyankenService_PlayServer) error {
 		player := util.Player(request.GetPlayer())
 		switch request.GetAction().(type) {
 		case *pb.PlayerRequest_Start:
-			err = g.start(server, roomID, player)
+			err = g.start(server,int(roomID))
 			if err != nil {
 				return err
 			}
 		case *pb.PlayerRequest_Zyanken:
-			err = g.zyanken(roomID, player)
+			err = g.zyanken(int(roomID), player)
 			if err != nil {
 				return err
 			}
@@ -47,21 +47,21 @@ func (g *GameHandler) Play(server pb.ZyankenService_PlayServer) error {
 	return nil
 }
 
-func (g *GameHandler) start(stream pb.ZyankenService_PlayServer, id int32, player *game.Player) error {
+func (g *GameHandler) start(stream pb.ZyankenService_PlayServer, id int) error {
 	g.Lock()
 	defer g.Unlock()
 
-	ga := g.games[int(id)]
+	ga := g.games[id]
 	if ga == nil {
 		ga = game.NewGame()
-		g.games[int(id)] = ga
-		g.client[int(id)] = make([]pb.ZyankenService_PlayServer, 0, 2)
+		g.games[id] = ga
+		g.client[id] = make([]pb.ZyankenService_PlayServer, 0, 2)
 	}
 
-	g.client[int(id)] = append(g.client[int(id)], stream)
+	g.client[id] = append(g.client[id], stream)
 
-	if len(g.client[int(id)]) == 2 {
-		for _, server := range g.client[int(id)] {
+	if len(g.client[id]) == 2 {
+		for _, server := range g.client[id] {
 			err := server.Send(&pb.PlayerResponse{Event: &pb.PlayerResponse_Ready{Ready: &pb.PlayerResponse_ReadyEvent{}}})
 			if err != nil {
 				return err
@@ -79,14 +79,14 @@ func (g *GameHandler) start(stream pb.ZyankenService_PlayServer, id int32, playe
 	return nil
 }
 
-func (g *GameHandler) zyanken(id int32, player *game.Player) error {
+func (g *GameHandler) zyanken(id int, player *game.Player) error {
 	g.Lock()
 	defer g.Unlock()
 
-	ga := g.games[int(id)]
+	ga := g.games[id]
 
 	winLose := ga.DecideWinLose(player.Te)
-	for _, server := range g.client[int(id)] {
+	for _, server := range g.client[id] {
 		err := server.Send(&pb.PlayerResponse{Event: &pb.PlayerResponse_Zyanken{Zyanken: &pb.PlayerResponse_ZyankenEvent{
 			Player: util.PBPlayer(player),
 		}}})
@@ -101,7 +101,7 @@ func (g *GameHandler) zyanken(id int32, player *game.Player) error {
 				},
 			}})
 
-			delete(g.client, int(id))
+			delete(g.client, id)
 			if err != nil {
 				return err
 			}
